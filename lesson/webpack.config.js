68b101cdf474a99461c70f879ca0bbc49aa5b33b
miaoruifeng <mrfnet@163.com>
2019-09-20 18:26:41 +0800

    webpack config

---
+++ lesson/webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path'); //引入名字叫path的node的核心模块
+
+module.exports = {
+    entry: './src/index.js', //打包入口文件 
+    output: {
+        filename: 'main.js', //打包文件存放目录
+        // 调用path模块的resolve方法，__dirname变量实际指的就是webpack.config.js所在的当前目录的路径
+        // 然后与dist结合 生成的路径就是bundle的绝对路径
+        // 如果是dist目录 则path不写也可以 默认会打包到dist目录下
+        path: path.resolve(__dirname, 'dist') //必须是绝对路径
+    }
+}
